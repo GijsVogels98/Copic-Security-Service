6b17ba5bcda8e15d8253128a08a4085083975d5c
Gijs <gijsvogels@hotmail.nl>
2021-01-11 16:31:10 +0100

    crossorigins

--- Copic-Security-Service/src/main/java/com/copic/securityservice/SecurityServiceApplication.java
+++ Copic-Security-Service/src/main/java/com/copic/securityservice/SecurityServiceApplication.java
@@ -7,7 +7,10 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+
+@CrossOrigin(origins = "*")
 @SpringBootApplication
 @EnableEurekaClient
 public class SecurityServiceApplication {
